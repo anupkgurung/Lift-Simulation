6ca5b8950c0c3c183e18440390fe028c00723280
anupkgurung <revivalzahead@gmail.com>
2022-07-16 02:12:30 +0530

    added few function

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+const addFloor = document.querySelector('#add-floor')
+const addLift = document.querySelector('#add-lift')
+let floorId=0;
+
+addFloor.addEventListener('click',function(){
+    addFloors()
+})
+
+addLift.addEventListener('click',function(){
+    
+})
+
+function createLift(){}
+
+function getFloorId(){
+    
+}
+function createFloor(){
+    const floorSection = document.querySelector('#floor-with-lift')
+    const floor = document.createElement('div')
+    floor.setAttribute('class','floor flex mt-1')
+    floor.setAttribute('id',0)
+}
+
+function createLiftBtn(){}
+
+function addFloors(){
+
+}
